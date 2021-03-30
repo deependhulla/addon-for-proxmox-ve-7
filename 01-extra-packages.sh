1c5b594ae2247b3521169c746cf660ae7e43d03f
Deepen Dhulla <deepen@deependhulla.com>
2021-03-30 20:13:29 +0530

    update 2021-03-30

---
+++ 01-extra-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+apt-get update
+
+## basic extra package for troubleshooting and data copy and mangement
+apt-get -y install git mc iptraf screen vim ethtool net-tools sshfs wget elinks dos2unix unbound dnsutils
+
+## php only cli tool for fast and easy scrpting
+apt-get -y install php-cli
+
+#command line sendemail tool
+apt-get -y install sendemail
+
+
