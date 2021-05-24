4969b803cb3a7173db719e9ef44f6c213c8bb257
Deepen Dhulla <deepen@deependhulla.com>
2021-05-24 10:55:23 +0530

    update 2021-05-24

--- 01-extra-packages.sh
+++ 01-extra-packages.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 ## basic extra package for troubleshooting and data copy and mangement
-apt-get -y install git mc iptraf screen vim ethtool net-tools sshfs wget elinks dos2unix unbound dnsutils
+apt-get -y install git mc iptraf screen vim ethtool net-tools sshfs wget elinks dos2unix dnsutils
 
 ## php only cli tool for fast and easy scrpting
 apt-get -y install php-cli
