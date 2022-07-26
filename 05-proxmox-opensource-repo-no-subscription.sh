13766a9e96e3846e92165c935d9ebc7874289e4c
Deepen Dhulla <deepen@deependhulla.com>
2022-07-26 11:31:56 +0530

    updated key
    updated key

--- 05-proxmox-opensource-repo-no-subscription.sh
+++ 05-proxmox-opensource-repo-no-subscription.sh
@@ -2,6 +2,8 @@
 
 echo "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-enterprise.list
 
+wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg 
+
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
