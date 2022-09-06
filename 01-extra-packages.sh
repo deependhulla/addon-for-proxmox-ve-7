3a67102614421a064f18ede696bf66aa8ee36464
Deepen Dhulla <deepen@deependhulla.com>
2022-09-06 18:02:22 +0530

    update 2022-09-06

--- 01-extra-packages.sh
+++ 01-extra-packages.sh
@@ -14,15 +14,29 @@ apt-get -y install php-cli
 apt-get -y install sendemail
 
 ### other Package if needed
-#apt-get install suricata liblocale-po-perl  libpve-network-perl ifupdown2 openvswitch-switch apt-transport-https
+#apt-get install liblocale-po-perl  libpve-network-perl ifupdown2 openvswitch-switch apt-transport-https
 #apt-get install debconf-utils pwgen iftop htop multitail lm-sensors
+
+## For Network Data-Traffic Recorder 
 #apt-get install vnstat vnstati
 
 ##for SDN Network
+## The Software Defined Network (SDN) feature allows you to create virtual networks (VNets) at the datacenter level.
 apt install libpve-network-perl ifupdown2 frr frr-pythontools
 echo "" >> /etc/network/interfaces
 echo "source /etc/network/interfaces.d/*" >> /etc/network/interfaces
 
+## VXLAN IPSEC Encryption
+## If you need to add encryption on top of a VXLAN, it’s possible to do so with IPSEC, through strongswan.
+## You’ll need to reduce the MTU by 60 bytes (IPv4) or 80 bytes (IPv6) to handle encryption.
+## So with default real 1500 MTU, you need to use a MTU of 1370 (1370 + 80 (IPSEC) + 50 (VXLAN) == 1500).
+#apt install strongswan
+
+##Suricata IPS integration
+##If you want to use the Suricata IPS (Intrusion Prevention System), it’s possible.
+##Packets will be forwarded to the IPS only after the firewall ACCEPTed them.
+# apt-get install suricata
+# modprobe nfnetlink_queue
 
 ## only if planned for cloud-init pve
 ##apt-get install cloud-init
